c56b9772021b61bdd48a3b222bfec76a47cfd7be
francisco franco <franciscofrancodev@gmail.com>
2024-10-09 21:57:21 -0300

    Adding inquiry to suppliers

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -15,8 +15,32 @@
         </div>
 
         <div class="informacao-pagina">
-            <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                Lista
+            <div style="width: 90%; margin-left: auto; margin-right: auto;">
+                <table border="1" width="100%">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Site</th>
+                            <th>UF</th>
+                            <th>Email</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($fornecedores as $fornecedor)
+                            <tr>
+                                <td>{{ $fornecedor->nome }}</td>
+                                <td>{{ $fornecedor->site }}</td>
+                                <td>{{ $fornecedor->uf }}</td>
+                                <td>{{ $fornecedor->email }}</td>
+                                <td>
+                                    <a href="#">Editar</a>
+                                    <a href="#">Excluir</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
